90a16ccec297847bd101038e06e1826302cfec67
Zalaya <enol.corzal@gmail.com>
2025-06-15 15:17:10 +0200

    enhancement: remove some environment variables and move the .gitignore

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 import tailwindcss from '@tailwindcss/vite'
+
 import path from 'path';
 
 export default defineConfig({
@@ -10,7 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src')
     }
   },
   server: {
